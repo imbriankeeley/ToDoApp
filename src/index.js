ff3e8b606df1c8baaef933411caaf96fbd5d29ae
Brian Keeley <imbriankeeley@gmail.com>
2024-09-15 11:06:24 -0400

    Recreated skeleton for original todo application

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+import './styles.css';
+
+// import { cardForm, addTask, submitTask, selectDetails, exampleDetails, closeExample, addTaskGeneral} from './task.js'
+// import { removeCard, removeExample, createExampleCard } from './dom.js'
+// import { addProject } from './project.js'
+// import { putExampleBack, loadStoredTasks, clearGeneralTasks, generalTasks } from './storage.js'
+
+library.add(fas, far, fab) 
+
+dom.i2svg() 
