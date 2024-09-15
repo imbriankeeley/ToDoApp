caf4a98fc2b9157a517616826a0cf5468ce743ad
Brian Keeley <imbriankeeley@gmail.com>
2024-09-15 12:17:05 -0400

    Added some more local storage functionality

--- src/index.js
+++ src/index.js
@@ -13,28 +13,35 @@ import './styles.css';
 // import { cardForm, addTask, submitTask, selectDetails, exampleDetails, closeExample, addTaskGeneral} from './task.js'
 // import { removeCard, removeExample, createExampleCard } from './dom.js'
 // import { addProject } from './project.js'
-// import { putExampleBack, loadStoredTasks, clearGeneralTasks, generalTasks } from './storage.js'
+import { projects, projectId, createProject, putExampleBack, loadStoredTasks, clearGeneralTasks, generalTasks, exampleTask } from './storage.js'
 
 library.add(fas, far, fab) 
 
 dom.i2svg() 
 
 
+createProject(projectId, 'General', []);
+
+if(localStorage.getItem('exampleRemove') === 'false') {
+    projects.find(obj => obj.id === 0).items.push(exampleTask);
+}
+
+
+// Intialize cards on to screen on refresh - check for example card validity
+// loadStoredTasks();
+
 //Add task to general project
-document.getElementById('taskButton').addEventListener('click', addTask);
+// document.getElementById('taskButton').addEventListener('click', addTask);
 
-//Get data from user after a successful sumbit and add card
-cardForm.addEventListener('submit', submitTask)
-
-
-//Select details for each task
-document.querySelectorAll('.details').forEach(button => {button.addEventListener('click', selectDetails)}) 
+// //Get data from user after a successful sumbit and add card
+// cardForm.addEventListener('submit', submitTask)
 
 
-
-//Add project
-document.getElementById('projectButton').addEventListener('click', addProject);
+// //Select details for each task
+// document.querySelectorAll('.details').forEach(button => {button.addEventListener('click', selectDetails)}) 
 
 
 
-console.log(localStorage)
+// //Add project
+// document.getElementById('projectButton').addEventListener('click', addProject);
+
--- src/storage.js
+++ src/storage.js
@@ -4,12 +4,12 @@ localStorage.setItem('projectId', 0);
 localStorage.setItem('taskId', 0);
 localStorage.setItem('exampleRemove', false);
 
-export let projectId = localStorage.getItem('projectId');
-export let taskId = localStorage.getItem('taskId');
+export let projectId = parseInt(localStorage.getItem('projectId'));
+export let taskId = parseInt(localStorage.getItem('taskId'));
 
 // Project array that will hold project objects
-const projects = [];
-localStorage.setItem('project', JSON.stringify(projects));
+export let projects = [];
+localStorage.setItem('projects', JSON.stringify(projects));
 
 export function AddProject(id, title, tasks) {
     this.id = id;
@@ -19,13 +19,13 @@ export function AddProject(id, title, tasks) {
 
 export function createProject(id, title, tasks) {
 
-    let newProject = AddProject(id, title, tasks);
+    let newProject = new AddProject(id, title, tasks);
 
     projects.push(newProject);
     localStorage.setItem('projects', JSON.stringify(projects));
 
-    let newId = projectId++;
-    localStorage.setItem('projectId', newId);
+    projectId++;
+    localStorage.setItem('projectId', projectId.toString());
     console.log(localStorage);
 };
 
@@ -36,4 +36,23 @@ export function AddTask(id, title, description, date) {
     this.description = description;
     this.date = date;
 };
+
+// Date
+let currentDate = new Date();
+//     currentDate.setHours(0, 0, 0, 0);
+//     date.setHours(0, 0, 0, 0);
+//     date.setDate(date.getDate());
+//     let day = date.getDate();
+//     let month = date.getMonth() + 1;
+//     let year = date.getFullYear();
+//     let formattedDate = `${month}/${day}/${year}`;
+
+// Create object for example task to push onto general project
+export const exampleTask = new AddTask(0, 'Example', 'This is an example task', currentDate)
+
+
+
+
+
+// Load stored tasks from general project onto screen on refresh - check for example card validity
 
